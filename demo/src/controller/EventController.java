172ac859fbbdba67caf0fee6457d85c9751863cb
mindPazi <gitmind87@gmail.com>
2025-02-02 16:58:55 +0100

    swagger

--- demo/src/controller/EventController.java
+++ demo/src/controller/EventController.java
@@ -1,3 +1,16 @@
+@RestController
+@RequestMapping("/api/events")
+@Tag(name = "Event Controller", description = "Gestione eventi")
 public class EventController {
+
+    @GetMapping
+    @Operation(summary = "Ottieni tutti gli eventi")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
 
 }
