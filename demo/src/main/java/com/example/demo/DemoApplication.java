86f0a111608c4b2a4f54abbb3c03a5092a32fe12
mindPazi <gitmind87@gmail.com>
2025-02-02 18:00:21 +0100

    config, ma esclusi da demoapplication

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example", excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.example\\.config\\..*"),
+		@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.example\\.dao\\..*")
+})
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
